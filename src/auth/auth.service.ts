dfe8480ef64699d04edcc13c3e9dc5379953e563
Matias <matias_corvalan@outlook.com>
2020-07-22 00:18:49 -0300

    auth/signUp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UserRpository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +9,10 @@ export class AuthService {
         @InjectRepository(UserRpository)
         private userRepository: UserRpository,
     ) {}
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.signUp(authCredentialsDto);
+
+    }
 
 }
