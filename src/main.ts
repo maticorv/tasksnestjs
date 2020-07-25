57e5f6a49c0d63e0fdbbe704e3a64073bd2e69cf
Matias <matias_corvalan@outlook.com>
2020-07-24 21:03:28 -0300

    habilitacion de cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as config from 'config';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = process.env.PORT || config.get('server').get('port');
 
   await app.listen(port);
